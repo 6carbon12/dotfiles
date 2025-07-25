c5858090ac9e9ea2b08624b34c34c0f1c9e70ff7
Kiran S <kiran.shantharaju@gmail.com>
2025-07-25 22:35:00 +0530

    Added a cooler dashboard fro alpha-nvim

--- cli/nvim/lua/plugins/alpha-nvim.lua
+++ cli/nvim/lua/plugins/alpha-nvim.lua
@@ -2,7 +2,44 @@ return {
   'goolord/alpha-nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function ()
-    require'alpha'.setup(require'alpha.themes.dashboard'.config)
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.dashboard")
+
+    dashboard.section.header.val = {
+      "                                                  ██████╗██████╗ ██████╗                                            ",
+      "                                                 ██╔════╝██╔══██╗██╔══██╗                                           ",
+      "                                                 ██║     ██████╔╝██████╔╝                                           ",
+      "                                                 ██║     ██╔═══╝ ██╔═══╝                                            ",
+      "                                                 ╚██████╗██║     ██║                                                ",
+      "                                                  ╚═════╝╚═╝     ╚═╝                                                ",
+      " ██████╗ ██████╗ ██████╗ ██╗   ██╗    ██████╗  █████╗ ███████╗████████╗███████╗    ██████╗ ██████╗  █████╗ ██╗   ██╗",
+      "██╔════╝██╔═══██╗██╔══██╗╚██╗ ██╔╝    ██╔══██╗██╔══██╗██╔════╝╚══██╔══╝██╔════╝    ██╔══██╗██╔══██╗██╔══██╗╚██╗ ██╔╝",
+      "██║     ██║   ██║██████╔╝ ╚████╔╝     ██████╔╝███████║███████╗   ██║   █████╗      ██████╔╝██████╔╝███████║ ╚████╔╝ ",
+      "██║     ██║   ██║██╔═══╝   ╚██╔╝      ██╔═══╝ ██╔══██║╚════██║   ██║   ██╔══╝      ██╔═══╝ ██╔══██╗██╔══██║  ╚██╔╝  ",
+      "╚██████╗╚██████╔╝██║        ██║       ██║     ██║  ██║███████║   ██║   ███████╗    ██║     ██║  ██║██║  ██║   ██║   ",
+      " ╚═════╝ ╚═════╝ ╚═╝        ╚═╝       ╚═╝     ╚═╝  ╚═╝╚══════╝   ╚═╝   ╚══════╝    ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ",
+      "                                                                                                                    ",
+    }
+
+
+    dashboard.section.buttons.val = {
+      dashboard.button("e", "  New file", ":enew<CR>"),
+      dashboard.button("r", "  Recent files", ":Telescope oldfiles<CR>"),
+      dashboard.button("l", "  Restore session", ":lua require('persistence').load()<CR>"),
+      dashboard.button("q", "  Quit", ":qa<CR>"),
+    }
+
+    dashboard.section.footer.val = {
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "Win if u can, lose if u must, BUT always cheat",
+    }
+
+    alpha.setup(dashboard.opts)
   end
 }
 
