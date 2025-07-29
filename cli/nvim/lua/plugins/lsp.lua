da2ae070235996c80111dbb58fe025a10deaf7ab
Kiran S <6carbon12.oss@proton.me>
2025-07-29 17:50:13 +0530

    Added gd and gD keymaps in C/C++ context

--- cli/nvim/lua/plugins/lsp.lua
+++ cli/nvim/lua/plugins/lsp.lua
@@ -14,6 +14,13 @@ return {
     lspconfig.emmet_ls.setup({
       filetypes = { "html", "css", "javascriptreact", "typescriptreact" },
     })
+    lspconfig.clangd.setup({
+      on_attach = function(_, bufnr)
+        local opts = { noremap = true, silent = true, buffer = bufnr }
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+      end
+    })
   end,
 }
 
