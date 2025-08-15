3d8905eadf40563317b2cc4fa26c7737c4d7e9ed
Kiran S <6carbon12.oss@proton.me>
2025-08-15 17:26:35 +0530

    Remove `tag`

--- cli/nvim/lua/plugins/telescope.lua
+++ cli/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,5 @@
 return {
   'nvim-telescope/telescope.nvim',
-  tag = '0.1.x', -- Recommended to pin to a stable version
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
     -- This is where you would add configuration options for telescope,
