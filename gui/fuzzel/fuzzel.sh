4d26c65d87cd4c4f151ecf2281317a2b9a12d970
Kiran S <6carbon12.oss@proton.me>
2025-08-15 12:22:01 +0530

    Use bash to run the script and Fix btop

--- gui/fuzzel/fuzzel.sh
+++ gui/fuzzel/fuzzel.sh
@@ -1,5 +1,4 @@
-#!/bin/zsh
-
+#!/bin/bash
 # This is a custom script to launch my apps using fuzzel dmenu
 
 # All the shown choices, Icons are shown via NFs
@@ -20,7 +19,7 @@ selection=$(printf "%s\n" "$choices" | fuzzel --dmenu --prompt :)
 # Launching the apps if they are selected
 case "$selection" in
     *Kitty) gtk-launch kitty ;;
-    *btop) kitty -e btop ;;
+    *BTOP) kitty -e btop ;;
     *Firefox) gtk-launch firefox ;;
     *Pavucontrol) gtk-launch org.pulseaudio.pavucontrol ;;
     *VirtualBox) gtk-launch virtualbox ;;
