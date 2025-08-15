82c6b07ee0a8666a30529068106c06df17ace35f
Kiran S <6carbon12.oss@proton.me>
2025-08-15 17:24:19 +0530

    QOL changes for nvimtree
    - Add correct regex for .git/
    - Hijack cursor for predictibility
    - Increase width
    - Add guides
    - Remove folder arrows

--- cli/nvim/lua/plugins/nvimtree.lua
+++ cli/nvim/lua/plugins/nvimtree.lua
@@ -5,19 +5,36 @@ return {
   },
   config = function()
     require("nvim-tree").setup({
-      filters = {
-        custom = {
-          "^.git$"
-        }
+      hijack_cursor = true,        -- cursor stays on file when opening
+      update_focused_file = {
+        enable = true,             -- auto-focus file in tree if it is in cwd
+        update_root = false,       -- don't change cwd to focus the file
       },
+
       actions = {
         open_file = {
           quit_on_open = true,
         },
       },
+
       view = {
+        width = 35,
         side = "right",
-      }
+        preserve_window_proportions = true,
+      },
+
+      renderer = {
+        indent_markers = { enable = true }, -- guide lines for folders
+        icons = {
+          show = {
+            folder_arrow = false,
+          },
+        },
+      },
+
+      filters = {
+        custom = { "^\\.git$" },    -- hide .git folder
+      },
     })
   end,
 }
