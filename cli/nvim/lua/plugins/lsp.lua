8a44b60b6e8606967e3b20635939589c0608c523
Kiran S <6carbon12.oss@proton.me>
2025-11-19 20:22:31 +0530

    [cli/nvim] Update lsp config
    - Change from depricated require("lspconfig") to vim.lsp.config(...)

--- cli/nvim/lua/plugins/lsp.lua
+++ cli/nvim/lua/plugins/lsp.lua
@@ -1,8 +1,7 @@
 return {
   "neovim/nvim-lspconfig",
   config = function()
-    local lspconfig = require("lspconfig")
-    lspconfig.lua_ls.setup({
+    vim.lsp.config("lua_ls", {
       settings = {
         Lua = {
           diagnostics = {
@@ -11,7 +10,7 @@ return {
         },
       },
     })
-    lspconfig.clangd.setup({
+    vim.lsp.config("cland", {
       on_attach = function(_, bufnr)
         local opts = { noremap = true, silent = true, buffer = bufnr }
         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
