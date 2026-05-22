eff35f569eaa40b40dff5d3bcf62e15a8ddda546
Kiran S <6carbon12.oss@proton.me>
2026-05-22 20:20:29 +0530

    [hypr] Scrolling workspace changes
    - Changed workspace two to use default layout instead of scrolling.
    - Made windows only take up 50% of the screen in scrolling workspaces.

--- config/hypr/conf/scrolling.lua
+++ config/hypr/conf/scrolling.lua
@@ -1,7 +1,7 @@
 hl.config({
   scrolling = {
-    fullscreen_on_one_column = true,
-    column_width = 0.95,
+    fullscreen_on_one_column = false,
+    column_width = 0.5,
     focus_fit_method = 0,
     explicit_column_widths = 0,
   }
